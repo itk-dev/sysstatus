9828251e7d41df18b49b4c55a0bf987175fedb62
Troels Ugilt Jensen <tujensen@gmail.com>
2018-12-06 11:13:25 +0100

    ITSTYR-47: Added calculations to excel sheet

--- src/Service/DataExporter.php
+++ src/Service/DataExporter.php
@@ -8,18 +8,17 @@ use App\Entity\System;
 use App\Repository\ReportRepository;
 use App\Repository\SystemRepository;
 use App\Repository\ThemeRepository;
-use Box\Spout\Writer\WriterFactory;
-use Box\Spout\Common\Type;
-use Box\Spout\Writer\Style\StyleBuilder;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityRepository;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class DataExporter
 {
     protected $reportRepository;
     protected $systemRepository;
     protected $themeRepository;
-    protected $headerStyle;
 
     /**
      * DataExporter constructor.
@@ -36,33 +35,21 @@ class DataExporter
         $this->reportRepository = $reportRepository;
         $this->systemRepository = $systemRepository;
         $this->themeRepository = $themeRepository;
-        $this->headerStyle = (new StyleBuilder())
-            ->setFontBold()
-            ->setShouldWrapText(true)
-            ->build();
     }
 
-    /**
-     * Export an excel file of the given entity type.
-     *
-     * @param \Doctrine\ORM\EntityRepository $entityRepository
-     * @param $typeString
-     * @param $type
-     * @throws \Box\Spout\Common\Exception\IOException
-     * @throws \Box\Spout\Common\Exception\InvalidArgumentException
-     * @throws \Box\Spout\Common\Exception\UnsupportedTypeException
-     * @throws \Box\Spout\Writer\Exception\WriterNotOpenedException
-     */
-    public function exportEntities(
+    public function export(
         EntityRepository $entityRepository,
         $typeString,
         $type
     ) {
-        $writer = WriterFactory::create(Type::XLSX); // for XLSX files
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $spreadsheet->setActiveSheetIndex(0);
+
+        $filename = $typeString.'-'.date("Y-m-d-H_i_s");
 
         $entities = $entityRepository->findAll();
-
-        $writer->openToBrowser($typeString.'-'.date("Y-m-d-H_i_s").'.xlsx');
 
         $themesThatApply = [];
 
@@ -94,7 +81,7 @@ class DataExporter
 
         $answerColumns = [];
 
-        $categoryRow = ['Kategorier', '', '', '', ''];
+        $categoryRow = ['Kategorier', '', '', '', '', ''];
 
         foreach ($categories as $category) {
             $categoryRow[] = $category->getName();
@@ -113,76 +100,176 @@ class DataExporter
             }
         }
 
-        $writer->addRowWithStyle($categoryRow, $this->headerStyle);
+        foreach ($categoryRow as $key => $cell) {
+            $sheet->setCellValueByColumnAndRow($key + 1, 1, $cell);
+        }
 
-        $writer->addRowWithStyle(
-            array_merge(
-                [
-                    'Id',
-                    'Navn',
-                    'Status',
-                    'Gruppe',
-                    'Afdeling',
-                ],
-                $answerColumns
-            ),
-            $this->headerStyle
+        $metaDataColumnHeadings = [
+            'Id',
+            'Navn',
+            'Status',
+            'Gruppe',
+            'Afdeling',
+            'Tema',
+        ];
+
+        $calculationColumnHeadings = [
+            'Sum af svar',
+            'Antal spørgsmål',
+            'Resultat %'
+        ];
+
+        $headings = array_merge(
+            $metaDataColumnHeadings,
+            $answerColumns,
+            $calculationColumnHeadings
         );
 
+        foreach ($headings as $key => $cell) {
+            $sheet->setCellValueByColumnAndRow($key + 1, 2, $cell);
+        }
+
+        $styleArray = [
+            'font' => [
+                'bold' => true,
+            ],
+        ];
+
+        $sheet->getStyle('A1:'. $this->getColumnLetter(count($categoryRow) + 1) . '1')->applyFromArray($styleArray)->getAlignment()->setTextRotation(45);
+        $sheet->getStyle('A2:'. $this->getColumnLetter(count($headings) + 1) . '2')->applyFromArray($styleArray)->getAlignment()->setTextRotation(45);
+
+        $rowNr = 2;
+
         foreach ($entities as $entity) {
+            $rowNr++;
+            $columnNr = count($metaDataColumnHeadings);
+
             $answers = $entity->getAnswers();
 
             $questionColumns = [];
 
+            $cellsThatApply = 0;
+
             foreach ($categories as $category) {
+                $categoryApplies = $this->categoryAppliesToEntity($entity, $category);
+
                 foreach ($category->getQuestions() as $question) {
+                    $columnNr++;
+
                     $value = '';
 
-                    foreach ($answers as $answer) {
-                        if ($answer->getQuestion()->getId() == $question->getId(
-                            )) {
-                            if ($answer->getSmiley() == SmileyType::BLUE ||
-                                $answer->getSmiley() == SmileyType::GREEN) {
-                                $value = 2;
-                            } else {
-                                if ($answer->getSmiley(
-                                    ) == SmileyType::YELLOW) {
-                                    $value = 1;
+                    if ($categoryApplies) {
+                        foreach ($answers as $answer) {
+                            if ($answer->getQuestion()->getId() == $question->getId(
+                                )) {
+                                if ($answer->getSmiley() == SmileyType::BLUE ||
+                                    $answer->getSmiley() == SmileyType::GREEN) {
+                                    $value = 2;
                                 } else {
                                     if ($answer->getSmiley(
-                                        ) == SmileyType::RED ||
-                                        is_null($answer->getSmiley())
-                                    ) {
-                                        $value = 0;
+                                        ) == SmileyType::YELLOW) {
+                                        $value = 1;
+                                    } else {
+                                        if ($answer->getSmiley(
+                                            ) == SmileyType::RED ||
+                                            is_null($answer->getSmiley())
+                                        ) {
+                                            $value = 0;
+                                        }
                                     }
                                 }
-                            }
 
-                            break;
+                                break;
+                            }
                         }
+
+                        if ($value == '') {
+                            $value = 0;
+                        }
+
+                        $cellsThatApply++;
                     }
 
                     $questionColumns[] = $value;
                 }
             }
 
-            $writer->addRow(
-                array_merge(
-                    [
-                        $entity->getSysInternalId(),
-                        $entity->getSysTitle(),
-                        $entity->getSysStatus(),
-                        $entity->getGroup()->getName(),
-                        $entity->getSysOwnerSub(),
-                    ],
-                    $questionColumns
-                )
-            );
+            $calculationColumns = [
+                '=SUM(' . $this->getColumnLetter(count($metaDataColumnHeadings)) . $rowNr . ':' .
+                $this->getColumnLetter($columnNr - 1) . $rowNr . ')',
+                $cellsThatApply,
+                '=((' . $this->getColumnLetter($columnNr) . $rowNr . ' / 2)/' . $this->getColumnLetter($columnNr + 1) . $rowNr . ')* 100'
+            ];
+
+            foreach (array_merge(
+                         [
+                             $entity->getSysInternalId(),
+                             $entity->getSysTitle(),
+                             $entity->getSysStatus(),
+                             $entity->getGroup()->getName(),
+                             $entity->getSysOwnerSub(),
+                             $entity->getTheme(),
+                         ],
+                         $questionColumns,
+                         $calculationColumns
+                     ) as $key => $cell) {
+                $sheet->setCellValueByColumnAndRow($key + 1, $rowNr, $cell);
+            }
         }
 
-        $writer->close();
-        // https://github.com/box/spout/issues/168#issuecomment-197276897
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="'.$filename.'.xlsx"');
+        header('Cache-Control: max-age=0');
+
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+
+        $writer->save('php://output');
         exit;
+    }
+
+    /**
+     * Test if a category applies to an entity
+     *
+     * @param $entity
+     * @param $category
+     * @return bool
+     */
+    private function categoryAppliesToEntity($entity, $category) {
+        $theme = $entity->getTheme();
+
+        if (is_null($theme)) {
+            return false;
+        }
+
+        foreach ($theme->getThemeCategories() as $themeCategory) {
+            if ($themeCategory->getCategory() == $category) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get Excel column letter.
+     *
+     * @param $columnNr
+     * @return string
+     */
+    private function getColumnLetter($columnNr) {
+        $res = '';
+
+        $range = range('A','Z');
+        $countRange = count($range);
+
+        if ($columnNr > $countRange) {
+            $res = $range[intval($columnNr / $countRange) - 1];
+        }
+
+        $p = $columnNr % $countRange;
+
+        $lastLetter = $range[$p];
+
+        return $res . $lastLetter;
     }
 
     /**
@@ -190,8 +277,7 @@ class DataExporter
      */
     public function exportReport()
     {
-        $this->exportEntities(
-            $this->reportRepository,
+        $this->export( $this->reportRepository,
             'reports',
             Report::class
         );
@@ -202,7 +288,7 @@ class DataExporter
      */
     public function exportSystem()
     {
-        $this->exportEntities(
+        $this->export(
             $this->systemRepository,
             'systems',
             System::class
