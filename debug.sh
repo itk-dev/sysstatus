4e988a06913e9163aa0b20f461cac8645477067d
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2023-03-07 13:53:14 +0100

    ITSTYR-81 / 1. Installed new symfony project / copied new docker files over / FIRST COMMIT

--- debug.sh
+++
@@ -1,1 +0,0 @@
-XDEBUG_CONFIG="idekey=PHPSTORM remote_enable=1 remote_mode=req remote_port=9000 remote_host=127.0.0.1 remote_connect_back=0" APP_FRONT_CONTROLLER=web/app_dev.php php -S localhost:8000 -t public
--- templates/svg/font-awesome-info.md
+++
@@ -1,5 +0,0 @@
-See https://fontawesome.com/license/free for
-check-circle-solid.svg
-exclamation-circle-solid.svg
-info-circle-solid.svg
-question-circle-solid.svg
