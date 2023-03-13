fa557ee548299cb775763688586927a6742a20ca
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2023-03-13 10:26:10 +0100

    ITSTYR-81 updated src/Entity/SelfServiceAvailableFromItem

--- src/Entity/SelfServiceAvailableFromItem.php
+++ src/Entity/SelfServiceAvailableFromItem.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SelfServiceAvailableFromItemRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SelfServiceAvailableFromItemRepository::class)]
@@ -13,8 +15,55 @@ class SelfServiceAvailableFromItem
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToMany(targetEntity: System::class, inversedBy: 'selfServiceAvailableFromItems')]
+    private Collection $systems;
+
+    public function __construct()
+    {
+        $this->systems = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, System>
+     */
+    public function getSystems(): Collection
+    {
+        return $this->systems;
+    }
+
+    public function addSystem(System $system): self
+    {
+        if (!$this->systems->contains($system)) {
+            $this->systems->add($system);
+        }
+
+        return $this;
+    }
+
+    public function removeSystem(System $system): self
+    {
+        $this->systems->removeElement($system);
+
+        return $this;
     }
 }
--- src/Entity/System.php
+++ src/Entity/System.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SystemRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SystemRepository::class)]
@@ -13,8 +15,43 @@ class System
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToMany(targetEntity: SelfServiceAvailableFromItem::class, mappedBy: 'systems')]
+    private Collection $selfServiceAvailableFromItems;
+
+    public function __construct()
+    {
+        $this->selfServiceAvailableFromItems = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, SelfServiceAvailableFromItem>
+     */
+    public function getSelfServiceAvailableFromItems(): Collection
+    {
+        return $this->selfServiceAvailableFromItems;
+    }
+
+    public function addSelfServiceAvailableFromItem(SelfServiceAvailableFromItem $selfServiceAvailableFromItem): self
+    {
+        if (!$this->selfServiceAvailableFromItems->contains($selfServiceAvailableFromItem)) {
+            $this->selfServiceAvailableFromItems->add($selfServiceAvailableFromItem);
+            $selfServiceAvailableFromItem->addSystem($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSelfServiceAvailableFromItem(SelfServiceAvailableFromItem $selfServiceAvailableFromItem): self
+    {
+        if ($this->selfServiceAvailableFromItems->removeElement($selfServiceAvailableFromItem)) {
+            $selfServiceAvailableFromItem->removeSystem($this);
+        }
+
+        return $this;
     }
 }
