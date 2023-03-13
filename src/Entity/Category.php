fded5f24aef851ff131c5400f5e64f79ce40de3a
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2023-03-13 14:58:33 +0100

    ITSTYR-81 Updated Entity Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,24 +6,40 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation\Loggable;
+use Gedmo\Mapping\Annotation\Versioned;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints\Unique;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[Loggable]
+#[UniqueEntity("name")]
 class Category
 {
+    use BlameableEntity;
+    use TimestampableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255, unique: true)]
+    #[Versioned]
+    private ?string $name = null;
+
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: ThemeCategory::class, orphanRemoval: true)]
     private Collection $themeCategories;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Question::class, cascade: ["persist"])]
+    private Collection $questions;
 
     public function __construct()
     {
         $this->themeCategories = new ArrayCollection();
+        $this->questions = new ArrayCollection();
     }
 
     public function __toString()
@@ -66,6 +82,20 @@ class Category
         return $this;
     }
 
+    /**
+     * Virtual.
+     */
+    public function getThemes() {
+        $list = [];
+        $iterator = $this->themeCategories->getIterator();
+
+        foreach($iterator as $i => $item) {
+            $list[$item->getTheme()->getId()] = $item->getTheme();
+        }
+
+        return $list;
+    }
+
     public function getName(): ?string
     {
         return $this->name;
@@ -74,6 +104,36 @@ class Category
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Question>
+     */
+    public function getQuestions(): Collection
+    {
+        return $this->questions;
+    }
+
+    public function addQuestion(Question $question): self
+    {
+        if (!$this->questions->contains($question)) {
+            $this->questions->add($question);
+            $question->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestion(Question $question): self
+    {
+        if ($this->questions->removeElement($question)) {
+            // set the owning side to null (unless already changed)
+            if ($question->getCategory() === $this) {
+                $question->setCategory(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -13,8 +13,23 @@ class Question
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToOne(inversedBy: 'questions')]
+    private ?Category $category = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
     }
 }
