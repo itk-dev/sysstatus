5e37f06509efb0b0a75f1475982c0bbaead24e4e
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2023-03-13 13:49:04 +0100

    ITSTYR-81 updated entity Theme

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GroupRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GroupRepository::class)]
@@ -14,8 +16,74 @@ class Group
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\ManyToMany(targetEntity: Theme::class, mappedBy: 'systemGroups')]
+    private Collection $systemThemes;
+
+    #[ORM\ManyToMany(targetEntity: Theme::class, mappedBy: 'reportGroups')]
+    private Collection $reportThemes;
+
+    public function __construct()
+    {
+        $this->systemThemes = new ArrayCollection();
+        $this->reportThemes = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Theme>
+     */
+    public function getSystemThemes(): Collection
+    {
+        return $this->systemThemes;
+    }
+
+    public function addSystemTheme(Theme $systemTheme): self
+    {
+        if (!$this->systemThemes->contains($systemTheme)) {
+            $this->systemThemes->add($systemTheme);
+            $systemTheme->addSystemGroup($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSystemTheme(Theme $systemTheme): self
+    {
+        if ($this->systemThemes->removeElement($systemTheme)) {
+            $systemTheme->removeSystemGroup($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Theme>
+     */
+    public function getReportThemes(): Collection
+    {
+        return $this->reportThemes;
+    }
+
+    public function addReportTheme(Theme $reportTheme): self
+    {
+        if (!$this->reportThemes->contains($reportTheme)) {
+            $this->reportThemes->add($reportTheme);
+            $reportTheme->addReportGroup($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReportTheme(Theme $reportTheme): self
+    {
+        if ($this->reportThemes->removeElement($reportTheme)) {
+            $reportTheme->removeReportGroup($this);
+        }
+
+        return $this;
     }
 }
--- src/Entity/Theme.php
+++ src/Entity/Theme.php
@@ -6,10 +6,22 @@ use App\Repository\ThemeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Blameable\Traits\BlameableEntity;
+use Gedmo\Mapping\Annotation\Loggable;
+use Gedmo\Mapping\Annotation\Versioned;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+
 
 #[ORM\Entity(repositoryClass: ThemeRepository::class)]
+#[Loggable]
+#[UniqueEntity('name')]
 class Theme
 {
+    use BlameableEntity;
+    use TimestampableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -18,9 +30,26 @@ class Theme
     #[ORM\OneToMany(mappedBy: 'theme', targetEntity: ThemeCategory::class, orphanRemoval: true)]
     private Collection $themeCategories;
 
+    #[ORM\ManyToMany(targetEntity: Group::class, inversedBy: 'systemThemes')]
+    private Collection $systemGroups;
+
+    #[ORM\ManyToMany(targetEntity: Group::class, inversedBy: 'reportThemes')]
+    private Collection $reportGroups;
+
+    #[ORM\Column(length: 255)]
+    #[Versioned]
+    private ?string $name = null;
+
     public function __construct()
     {
         $this->themeCategories = new ArrayCollection();
+        $this->systemGroups = new ArrayCollection();
+        $this->reportGroups = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+        return $this->getName() ?: $this->getId();
     }
 
     public function getId(): ?int
@@ -48,12 +77,91 @@ class Theme
 
     public function removeThemeCategory(ThemeCategory $themeCategory): self
     {
-        if ($this->themeCategories->removeElement($themeCategory)) {
-            // set the owning side to null (unless already changed)
-            if ($themeCategory->getTheme() === $this) {
-                $themeCategory->setTheme(null);
-            }
+
+
+        return $this;
+    }
+
+    /**        if ($this->themeCategories->removeElement($themeCategory)) {
+    // set the owning side to null (unless already changed)
+    if ($themeCategory->getTheme() === $this) {
+    $themeCategory->setTheme(null);
+    }
+    }
+     * Virtual.
+     */
+    public function getOrderedCategories() {
+        $list = [];
+
+        $themeCategories = $this->themeCategories;
+        $iterator = $themeCategories->getIterator();
+        $iterator->uasort(function ($first, $second) {
+            return (int) $first->getSortOrder() < (int) $second->getSortOrder() ? 1 : -1;
+        });
+
+        foreach($iterator as $i => $item) {
+            $list[] = $item->getCategory();
         }
+
+        return $list;
+    }
+
+    /**
+     * @return Collection<int, Group>
+     */
+    public function getSystemGroups(): Collection
+    {
+        return $this->systemGroups;
+    }
+
+    public function addSystemGroup(Group $systemGroup): self
+    {
+        if (!$this->systemGroups->contains($systemGroup)) {
+            $this->systemGroups->add($systemGroup);
+        }
+
+        return $this;
+    }
+
+    public function removeSystemGroup(Group $systemGroup): self
+    {
+        $this->systemGroups->removeElement($systemGroup);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Group>
+     */
+    public function getReportGroups(): Collection
+    {
+        return $this->reportGroups;
+    }
+
+    public function addReportGroup(Group $reportGroup): self
+    {
+        if (!$this->reportGroups->contains($reportGroup)) {
+            $this->reportGroups->add($reportGroup);
+        }
+
+        return $this;
+    }
+
+    public function removeReportGroup(Group $reportGroup): self
+    {
+        $this->reportGroups->removeElement($reportGroup);
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
 
         return $this;
     }
