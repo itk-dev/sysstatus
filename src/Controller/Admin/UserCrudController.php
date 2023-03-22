c3c00abac8bdc94c9c153d81d0097d28f23d567f
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2023-03-22 14:46:18 +0100

    added security bundle for users

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,9 +17,24 @@ class UserCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
+        $username = Field::new('username');
+        $email = Field::new('email');
+        $enabled = Field::new('enabled');
+        $lastLogin = Field::new('lastLogin');
+        $roles = Field::new('roles');
 
-        ];
+        if (Crud::PAGE_INDEX === $pageName) {
+            return [$username, $email, $enabled, $lastLogin, $roles ];
+        }
+//        elseif(Crud::PAGE_DETAIL === $pageName) {
+//            return [$title, $coll_answers_detail];
+//        }
+//        elseif(Crud::PAGE_NEW === $pageName) {
+//            return [$title, $editor_description, $editor_instructions, $editor_preparations, $coll_questions, $coll_configuration ];
+//        }
+//        elseif(Crud::PAGE_EDIT === $pageName) {
+//            return [$title, $editor_description, $editor_instructions, $editor_preparations, $coll_questions, $coll_configuration ];
+//        }
     }
 
 }
