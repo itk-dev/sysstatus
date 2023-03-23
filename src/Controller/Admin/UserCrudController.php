8067f6cb7cd1de9e3389be3b886e09ba387b26dd
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2023-03-23 09:43:43 +0100

    made chages to users

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,9 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -14,24 +20,39 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+
+
+        $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+
+        ;
+
+        return $actions;
+    }
 
     public function configureFields(string $pageName): iterable
     {
-        $username = Field::new('username');
-        $email = Field::new('email');
-        $enabled = Field::new('enabled');
-        $lastLogin = Field::new('lastLogin');
-        $roles = Field::new('roles');
+        $username = TextField::new('username');
+        $email = TextField::new('email');
+        $groups = CollectionField::new('groups');
+        $enabled = TextField::new('enabled');
+        $lastLogin = DateTimeField::new('lastLogin');
+        $roles = ArrayField::new('roles');
+
+
 
         if (Crud::PAGE_INDEX === $pageName) {
             return [$username, $email, $enabled, $lastLogin, $roles ];
         }
-//        elseif(Crud::PAGE_DETAIL === $pageName) {
-//            return [$title, $coll_answers_detail];
-//        }
-//        elseif(Crud::PAGE_NEW === $pageName) {
-//            return [$title, $editor_description, $editor_instructions, $editor_preparations, $coll_questions, $coll_configuration ];
-//        }
+        elseif(Crud::PAGE_DETAIL === $pageName) {
+            return [$username, $email, $groups, $enabled, $lastLogin, $roles ];
+        }
+        elseif(Crud::PAGE_NEW === $pageName) {
+            return [$username, $email, $groups, $enabled, $lastLogin, $roles ];
+        }
 //        elseif(Crud::PAGE_EDIT === $pageName) {
 //            return [$title, $editor_description, $editor_instructions, $editor_preparations, $coll_questions, $coll_configuration ];
 //        }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Blameable\Traits\BlameableEntity;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
@@ -24,6 +25,21 @@ class User
     #[ORM\ManyToMany(targetEntity: Group::class, inversedBy: 'users')]
     #[ORM\JoinTable(name: "fos_user_user_group" )]
     private Collection $groups;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?bool $enabled = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $lastLogin = null;
+
+    #[ORM\Column]
+    private array $roles = [];
 
     public function __construct()
     {
@@ -63,6 +79,66 @@ class User
     public function removeGroup(Group $group): self
     {
         $this->groups->removeElement($group);
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function isEnabled(): ?bool
+    {
+        return $this->enabled;
+    }
+
+    public function setEnabled(bool $enabled): self
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    public function getLastLogin(): ?\DateTimeInterface
+    {
+        return $this->lastLogin;
+    }
+
+    public function setLastLogin(\DateTimeInterface $lastLogin): self
+    {
+        $this->lastLogin = $lastLogin;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
