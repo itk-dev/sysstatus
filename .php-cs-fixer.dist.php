673dd65f687057c45dee1dbd59746ad288c03429
Mr-Martin-Kristiansen <krimart@aarhus.dk>
2024-08-20 15:22:42 +0200

    added  rules to php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,17 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
+    ->exclude(['var', 'vendor', 'node_modules'])
+
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'multiline_whitespace_before_semicolons' => ['strategy' => 'new_line_for_chained_calls'],
+        // @see https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/rules/import/ordered_imports.rst
+        'ordered_imports' => ['imports_order' => ['const', 'class', 'function']],
     ])
+
     ->setFinder($finder)
 ;
