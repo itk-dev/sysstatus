18c8768f4f797659a5cc58db5a52ebc131d0c2fe
Mr-Martin-Kristiansen <krimart@aarhus.dk>
2024-09-13 09:33:12 +0200

    standpoint 13september

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -9,7 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategoryType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name', null, [
@@ -17,6 +17,7 @@ class CategoryType extends AbstractType
                     'class' => 'form-control'
                 ]
             ])
+
         ;
     }
 
