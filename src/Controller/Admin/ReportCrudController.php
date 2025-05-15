a9a853e75d3c35846e351fee8a84fe569413a3a2
Mikkel Ricky <rimi@aarhus.dk>
2025-05-15 14:25:44 +0200

    4410: Cleaned up field references

--- src/Controller/Admin/ReportCrudController.php
+++ src/Controller/Admin/ReportCrudController.php
@@ -47,7 +47,7 @@ class ReportCrudController extends AbstractCrudController
                     ->renderExpanded();
                 yield TextField::new('sysOwnerSub')->setLabel('entity.report.sys_owner_sub');
                 yield TextField::new('sysSystemOwner')->setLabel('entity.report.sys_system_owner');
-                yield UrlField::new('sysLink')->setLabel('entity.report.sys_id')->setLabel('entity.report.sys_link')
+                yield UrlField::new('sysLink')->setLabel('entity.report.sys_link')
                     ->formatValue(static fn ($value) => new TranslatableMessage('Link'));
                 yield BooleanField::new('textSet')->setLabel('entity.report.text')
                     ->renderAsSwitch(false)
@@ -64,45 +64,45 @@ class ReportCrudController extends AbstractCrudController
                 yield TextField::new('name')->setLabel('entity.report.name');
                 yield TextEditorField::new('text')->setLabel('entity.report.text')
                     // Show raw value
-                    ->setTemplatePath('admin/text_editor.raw.html.twig')
-                ;
-                yield TextField::new('sys_system_owner')->setLabel('entity.report.sys_system_owner');
+                    ->setTemplatePath('admin/text_editor.raw.html.twig');
+                yield TextField::new('sysSystemOwner')->setLabel('entity.report.sys_system_owner');
                 // @todo Add links to each group?
                 yield CollectionField::new('groups')->setLabel('entity.report.groups');
-                yield CollectionField::new('answerarea')->setTemplatePath('easy_admin_overrides/answers_show.html.twig')->setLabel('entity.report.answers');
-                yield TextField::new('sys_alternative_title')->setLabel('entity.report.sys_alternative_title');
-                yield DateTimeField::new('sys_updated')->setLabel('entity.report.sys_updated');
-                yield TextField::new('sys_owner')->setLabel('entity.report.sys_owner');
-                yield BooleanField::new('sys_confidential_information')->setLabel('entity.report.sys_confidential_information');
-                yield TextField::new('sys_purpose')->setLabel('entity.report.sys_purpose');
-                yield TextField::new('sys_classification')->setLabel('entity.report.sys_classification');
-                yield DateTimeField::new('sys_date_for_revision')->setLabel('entity.report.sys_date_for_revision');
-                yield TextField::new('sys_persons')->setLabel('entity.report.sys_persons');
-                yield TextField::new('sys_information_types')->setLabel('entity.report.sys_information_types');
-                yield TextField::new('sys_data_sent_to')->setLabel('entity.report.sys_data_sent_to');
-                yield TextField::new('sys_data_come_from')->setLabel('entity.report.sys_data_come_from');
-                yield TextField::new('sys_data_location')->setLabel('entity.report.sys_data_location');
-                yield TextField::new('sys_latest_deletion_date')->setLabel('entity.report.sys_latest_deletion_date');
-                yield TextField::new('sys_data_worth_saving')->setLabel('entity.report.sys_data_worth_saving');
-                yield TextField::new('sys_data_processors')->setLabel('entity.report.sys_data_processors');
-                yield TextField::new('sys_data_processing_agreement')->setLabel('entity.report.sys_data_processing_agreement');
-                yield TextField::new('sys_data_processing_agreement_link')->setLabel('entity.report.sys_data_processing_agreement_link');
-                yield TextField::new('sys_auditor_statement')->setLabel('entity.report.sys_auditor_statement');
-                yield TextField::new('sys_auditor_statement_link')->setLabel('entity.report.sys_auditor_statement_link');
-                yield TextField::new('sys_data_to_science')->setLabel('entity.report.sys_data_to_science');
-                yield TextField::new('sys_usage')->setLabel('entity.report.sys_usage');
-                yield TextField::new('sys_request_for_insight')->setLabel('entity.report.sys_request_for_insight');
-                yield DateTimeField::new('sys_date_use')->setLabel('entity.report.sys_date_use');
-                yield TextField::new('sys_status')->setLabel('entity.report.sys_status');
-                yield TextField::new('sys_remarks')->setLabel('entity.report.sys_remarks');
-                yield TextField::new('sys_internal_information')->setLabel('entity.report.sys_internal_information');
-                yield TextField::new('sys_obligation_to_inform')->setLabel('entity.report.sys_obligation_to_inform');
-                yield TextField::new('sys_legal_basis')->setLabel('entity.report.sys_legal_basis');
-                yield TextField::new('sys_consent')->setLabel('entity.report.sys_consent');
-                yield TextField::new('sys_impact_analysis')->setLabel('entity.report.sys_impact_analysis');
-                yield TextField::new('sys_impact_analysis_link')->setLabel('entity.report.sys_impact_analysis_link');
-                yield TextField::new('sys_authorization_procedure')->setLabel('entity.report.sys_authorization_procedure');
-                yield TextField::new('sys_version')->setLabel('entity.report.sys_version');
+                yield CollectionField::new('answerArea')->setLabel('entity.report.answers')
+                    ->setTemplatePath('easy_admin_overrides/answers_show.html.twig');
+                yield TextField::new('sysAlternativeTitle')->setLabel('entity.report.sys_alternative_title');
+                yield DateTimeField::new('sysUpdated')->setLabel('entity.report.sys_updated');
+                yield TextField::new('sysOwner')->setLabel('entity.report.sys_owner');
+                yield BooleanField::new('sysConfidentialInformation')->setLabel('entity.report.sys_confidential_information');
+                yield TextField::new('sysPurpose')->setLabel('entity.report.sys_purpose');
+                yield TextField::new('sysClassification')->setLabel('entity.report.sys_classification');
+                yield DateTimeField::new('sysDateForRevision')->setLabel('entity.report.sys_date_for_revision');
+                yield TextField::new('sysPersons')->setLabel('entity.report.sys_persons');
+                yield TextField::new('sysInformationTypes')->setLabel('entity.report.sys_information_types');
+                yield TextField::new('sysDataSentTo')->setLabel('entity.report.sys_data_sent_to');
+                yield TextField::new('sysDataComeFrom')->setLabel('entity.report.sys_data_come_from');
+                yield TextField::new('sysDataLocation')->setLabel('entity.report.sys_data_location');
+                yield TextField::new('sysLatestDeletionDate')->setLabel('entity.report.sys_latest_deletion_date');
+                yield TextField::new('sysDataWorthSaving')->setLabel('entity.report.sys_data_worth_saving');
+                yield TextField::new('sysDataProcessors')->setLabel('entity.report.sys_data_processors');
+                yield TextField::new('sysDataProcessingAgreement')->setLabel('entity.report.sys_data_processing_agreement');
+                yield TextField::new('sysDataProcessingAgreementLink')->setLabel('entity.report.sys_data_processing_agreement_link');
+                yield TextField::new('sysAuditorStatement')->setLabel('entity.report.sys_auditor_statement');
+                yield TextField::new('sysAuditorStatementLink')->setLabel('entity.report.sys_auditor_statement_link');
+                yield TextField::new('sysDataToScience')->setLabel('entity.report.sys_data_to_science');
+                yield TextField::new('sysUsage')->setLabel('entity.report.sys_usage');
+                yield TextField::new('sysRequestForInsight')->setLabel('entity.report.sys_request_for_insight');
+                yield DateTimeField::new('sysDateUse')->setLabel('entity.report.sys_date_use');
+                yield TextField::new('sysStatus')->setLabel('entity.report.sys_status');
+                yield TextField::new('sysRemarks')->setLabel('entity.report.sys_remarks');
+                yield TextField::new('sysInternalInformation')->setLabel('entity.report.sys_internal_information');
+                yield TextField::new('sysObligationToInform')->setLabel('entity.report.sys_obligation_to_inform');
+                yield TextField::new('sysLegalBasis')->setLabel('entity.report.sys_legal_basis');
+                yield TextField::new('sysConsent')->setLabel('entity.report.sys_consent');
+                yield TextField::new('sysImpactAnalysis')->setLabel('entity.report.sys_impact_analysis');
+                yield TextField::new('sysImpactAnalysisLink')->setLabel('entity.report.sys_impact_analysis_link');
+                yield TextField::new('sysAuthorizationProcedure')->setLabel('entity.report.sys_authorization_procedure');
+                yield TextField::new('sysVersion')->setLabel('entity.report.sys_version');
 
                 return;
 
