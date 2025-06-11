bf466aa594fafbec2ae61cae78bb4dd0d7b0e9d0
Mikkel Ricky <rimi@aarhus.dk>
2025-06-11 11:34:01 +0200

    4410: Fixed entity active filter

--- src/Doctrine/EntityActiveFilter.php
+++ src/Doctrine/EntityActiveFilter.php
@@ -2,6 +2,8 @@
 
 namespace App\Doctrine;
 
+use App\Entity\Report;
+use App\Entity\System;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Query\Filter\SQLFilter;
 
@@ -9,8 +11,11 @@ class EntityActiveFilter extends SQLFilter
 {
     public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias): string
     {
-        if ($targetEntity->hasField('sysStatus')) {
-            return sprintf('%s.sys_status = %s', $targetTableAlias, $this->getParameter('active_value'));
+        switch ($targetEntity->getName()) {
+            case Report::class:
+                return sprintf('%s.sys_status = %s', $targetTableAlias, $this->getConnection()->quote('Aktiv'));
+            case System::class:
+                return sprintf('%s.sys_status <> %s', $targetTableAlias, $this->getConnection()->quote('Systemet bruges ikke længere'));
         }
 
         return '';
